517f9af01b8aea0ea1de3214aa6ea5aaee7f15a0
carla-ng <carlitashata@gmail.com>
2022-05-22 20:37:10 +0200

    Incrementing Counter.

---
+++ 15-incrementing-counter/js/script.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach( counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const targetNum = +counter.getAttribute('data-target')  // the + sign turns the string into a number. can also user parseInt or Number
+        const counterNum = +counter.innerText
+
+        const increment = targetNum / 200
+
+        if (counterNum < targetNum) {
+            counter.innerText = `${ Math.ceil( counterNum + increment ) }`
+            setTimeout( updateCounter, 1)
+        } else {
+            counter.innerText = targetNum
+        }
+    }
+
+    updateCounter()
+})
