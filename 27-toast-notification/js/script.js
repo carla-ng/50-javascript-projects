23fedd265dedaf3b605ed208b9251d943a6f0b51
carla-ng <carlitashata@gmail.com>
2023-01-14 02:34:28 +0100

    Toast Notification.

---
+++ 27-toast-notification/js/script.js
@@ -0,0 +1,42 @@
+const button = document.getElementById('button')
+const toasts = document.getElementById('toasts')
+
+const messages = [
+    'Message One',
+    'Message Two',
+    'Message Three',
+    'Message Four',
+]
+const types = ['info', 'success', 'error']
+
+
+// Listerners
+button.addEventListener('click', () => createNotification())
+
+
+// Create a notifications
+function createNotification(message = null, type = null) {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+    notif.classList.add(type ? type : getRandomType())
+
+    notif.innerText = message ? message : getRandomMessage()
+
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+    }, 3000)
+}
+
+
+// Get one of the four rnadom messages
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
+}
+
+
+// Get a random type 
+function getRandomType() {
+    return types[Math.floor(Math.random() * types.length)]
+}
