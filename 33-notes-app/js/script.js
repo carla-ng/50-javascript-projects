a071bf972f5d56cc07ef8cd59ff95ea80089e26b
carla-ng <carlitashata@gmail.com>
2023-01-22 19:50:14 +0100

    Notes App.

---
+++ 33-notes-app/js/script.js
@@ -0,0 +1,70 @@
+const addBtn = document.getElementById('add')
+const notes  = JSON.parse(localStorage.getItem('notes'))
+
+// Check if there are notes in LocalStorage
+if ( notes ) {
+    notes.forEach(note => addNewNote(note))
+}
+
+
+// Add note listener
+addBtn.addEventListener('click', () => addNewNote())
+
+
+// Add note
+function addNewNote(text = '') {
+    const note = document.createElement('div')
+    note.classList.add('note')
+
+    note.innerHTML = `
+                    <div class="tools">
+                        <button class="edit"><i class="fas fa-edit"></i></button>
+                        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+                    </div>
+                    <div class="main ${text ? "" : "hidden"}"></div>
+                    <textarea class="${text ? "hidden" : ""}"></textarea>
+                    `
+
+    const editBtn = note.querySelector('.edit')
+    const deleteBtn = note.querySelector('.delete')
+    const main = note.querySelector('.main')
+    const textArea = note.querySelector('textarea')
+
+    textArea.value = text
+    main.innerHTML = marked(text)
+
+    // Delete note
+    deleteBtn.addEventListener('click', () => {
+        note.remove()
+
+        updateLocalStorage()
+    })
+
+    // Edit note
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden')
+        textArea.classList.toggle('hidden')
+    })
+
+    // Write note
+    textArea.addEventListener('input', (e) => {
+        const { value } = e.target
+
+        main.innerHTML = marked(value)
+
+        updateLocalStorage()
+    })
+
+    document.body.appendChild(note)
+}
+
+
+// Update LocalStorage of notes
+function updateLocalStorage() {
+    const notesText = document.querySelectorAll('textarea')
+    const notes = []
+
+    notesText.forEach(note => notes.push(note.value))
+
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
